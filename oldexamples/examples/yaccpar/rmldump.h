7699eb862f34f5083b99b71dc96fa944350896fd
adrpo <adrpo@324bd646-64f9-0310-949b-d3e75d9ef174>
2006-03-22 00:27:00 +0000

    Removed file/folder
    git-svn-id: https://openmodelica.org/svn/MetaModelica/trunk@71 324bd646-64f9-0310-949b-d3e75d9ef174

--- oldexamples/examples/yaccpar/rmldump.h
+++
@@ -1,5 +0,0 @@
-/* interface RMLDump */
-extern void RMLDump_5finit(void);
-extern RML_FORWARD_LABEL(RMLDump__dump);
-extern RML_FORWARD_LABEL(RMLDump__dump_5ftype);
-extern RML_FORWARD_LABEL(RMLDump__print_5fid);
--- oldexamples/examples/yaccpar/rmltomod.h
+++
@@ -1,3 +0,0 @@
-/* interface RMLToMod */
-extern void RMLToMod_5finit(void);
-extern RML_FORWARD_LABEL(RMLToMod__transform);
