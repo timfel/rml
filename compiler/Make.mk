d083a9db03c2e1b8101054d36eface8bfe08d549
adrpo <adrpo@324bd646-64f9-0310-949b-d3e75d9ef174>
2008-06-26 20:35:37 +0000

    - some makes complain about:   "Make.mk:4: *** commands commence before first target.  Stop." - fix for this problem.
    git-svn-id: https://openmodelica.org/svn/MetaModelica/trunk@140 324bd646-64f9-0310-949b-d3e75d9ef174

--- compiler/Make.mk
+++ compiler/Make.mk
@@ -1,9 +1,9 @@
 # Makefile for the rml compiler and interpreter
 SHELL=/bin/sh
 ifeq (${SMLNJ_HOME},)
-	echo SMLNJ_HOME is not defined! 
-	echo Assuming SMLNJ_HOME=/usr/lib/smlnj
-	echo Please define SMLNJ_HOME if is not /usr/lib/smlnj
+	$(warning no SMLNJ_HOME variable is not defined!)
+	$(warning Assuming SMLNJ_HOME=/usr/lib/smlnj)
+	$(warning Please define SMLNJ_HOME if is not /usr/lib/smlnj)
 	SMLNJ_HOME_=/usr/lib/smlnj
 else
 	SMLNJ_HOME_=${SMLNJ_HOME}
@@ -20,7 +20,7 @@ TEMP=$(GENERATED) rml.grm.desc mod.grm.desc persistent.grm.desc
 default:	rml.$(HEAP_SUFFIX)
 
 rml.$(HEAP_SUFFIX):	$(GENERATED) *.sml *.sig *.cm
-	SMLNJ_HOME=${SMLNJ_HOME_} ; export SMLNJ_HOME ; cat make.sml | $(SMLCM)
+	SMLNJ_HOME="${SMLNJ_HOME_}" ; export SMLNJ_HOME ; cat make.sml | $(SMLCM)
 	ls -l rml.$(HEAP_SUFFIX)
 
 install:	install-rml
@@ -29,22 +29,22 @@ install-rml:	rml.$(HEAP_SUFFIX)
 	$(GOROOT)/etc/install-sml rml $(PREFIX) "$(SMLCM)" $(HEAP_SUFFIX) $(TARGET) $(GOROOT)
 
 rml.lex.sml:	rml.lex
-	SMLNJ_HOME=${SMLNJ_HOME_} ; export SMLNJ_HOME ; $(MLLEX) rml.lex
+	SMLNJ_HOME="${SMLNJ_HOME_}" ; export SMLNJ_HOME ; $(MLLEX) rml.lex
 
 rml.grm.sml rml.grm.sig:	rml.grm
-	SMLNJ_HOME=${SMLNJ_HOME_} ; export SMLNJ_HOME ; $(MLYACC) rml.grm
+	SMLNJ_HOME="${SMLNJ_HOME_}" ; export SMLNJ_HOME ; $(MLYACC) rml.grm
 
 mod.lex.sml:	mod.lex
-	SMLNJ_HOME=${SMLNJ_HOME_} ; export SMLNJ_HOME ; $(MLLEX) mod.lex
+	SMLNJ_HOME="${SMLNJ_HOME_}" ; export SMLNJ_HOME ; $(MLLEX) mod.lex
 
 mod.grm.sml mod.grm.sig:	mod.grm
-	SMLNJ_HOME=${SMLNJ_HOME_} ; export SMLNJ_HOME ; $(MLYACC) mod.grm
+	SMLNJ_HOME="${SMLNJ_HOME_}" ; export SMLNJ_HOME ; $(MLYACC) mod.grm
 
 persistent.lex.sml:	persistent.lex
-	SMLNJ_HOME=${SMLNJ_HOME_} ; export SMLNJ_HOME ; $(MLLEX) persistent.lex
+	SMLNJ_HOME="${SMLNJ_HOME_}" ; export SMLNJ_HOME ; $(MLLEX) persistent.lex
 
 persistent.grm.sml persistent.grm.sig:	persistent.grm
-	SMLNJ_HOME=${SMLNJ_HOME_} ; export SMLNJ_HOME ; $(MLYACC) persistent.grm
+	SMLNJ_HOME="${SMLNJ_HOME_}" ; export SMLNJ_HOME ; $(MLYACC) persistent.grm
 
 Makefile:	$(GOROOT)/config.cache Make.mk
 	(echo include $(GOROOT)/config.cache; cat Make.mk) > Makefile
