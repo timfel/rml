c8b18f7d8ad1b427f503776db82ff837dcfbfd94
adrpo <adrpo@324bd646-64f9-0310-949b-d3e75d9ef174>
2010-10-15 07:30:31 +0000

    - use rml_prim_none instead of generating it on the fly.
    git-svn-id: https://openmodelica.org/svn/MetaModelica/trunk@170 324bd646-64f9-0310-949b-d3e75d9ef174

--- runtime/common/logicalvar.c
+++ runtime/common/logicalvar.c
@@ -2,11 +2,6 @@
 #include <stdio.h>
 #include "rml.h"
 
-#define RML_NONEHDR	RML_STRUCTHDR(0,0)
-#define RML_SOMEHDR	RML_STRUCTHDR(1,1)
-
-static const struct rml_header rml_prim_none = { RML_NONEHDR };
-
 RML_BEGIN_LABEL(RML__lvar_5fget)
 {
     if( RML_GETHDR(rmlA0) == RML_BOUNDHDR ) {
@@ -15,7 +10,7 @@ RML_BEGIN_LABEL(RML__lvar_5fget)
 	option->data[0] = RML_REFDATA(rmlA0);
 	rmlA0 = RML_TAGPTR(option);
     } else
-	rmlA0 = RML_TAGPTR(&rml_prim_none);
+	rmlA0 = rml_prim_none_tagged;
     RML_TAILCALLK(rmlSC);
 }
 RML_END_LABEL
