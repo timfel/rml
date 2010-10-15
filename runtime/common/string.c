e9318b3a11753507d544a687ad4ec0667bcb509f
adrpo <adrpo@324bd646-64f9-0310-949b-d3e75d9ef174>
2010-10-15 07:46:47 +0000

    - fixes for stringAppendList - check for pointer equality first in string equality
    git-svn-id: https://openmodelica.org/svn/MetaModelica/trunk@175 324bd646-64f9-0310-949b-d3e75d9ef174

--- runtime/common/string.c
+++ runtime/common/string.c
@@ -39,7 +39,10 @@ struct rml_string *rml_prim_mkstring(rml_uint_t nbytes, rml_uint_t nliveargs)
 /* p_stringeq.c */
 rml_sint_t rml_prim_stringeq(void *p, rml_uint_t qhdr, const char *q)
 {
-  return RML_GETHDR(p) == qhdr && !memcmp(RML_STRINGDATA(p), q, RML_HDRSTRLEN(qhdr));
+  /* strings pointed to by the same pointer are equal */
+  if( RML_STRINGDATA(p) == q ) { return 1; }
+  /* else do the normal thing and compare also the terminating '\0' */
+  return RML_GETHDR(p) == qhdr && !memcmp(RML_STRINGDATA(p), q, RML_HDRSTRLEN(qhdr) + 1);
 }
 
 /* str_append.c */
@@ -58,16 +61,26 @@ RML_END_LABEL
 /* str_append_list.c */
 RML_BEGIN_LABEL(RML__string_5fappend_5flist)
 {
-  /* count the length of elements in the first list */
+  /* count the length of elements in the list */
   rml_uint_t len_car = 0;
   rml_uint_t len_cur = 0;
   rml_uint_t len = 0;
+  struct rml_string *str = 0;
   void *lst = rmlA0;
   while( RML_GETHDR(lst) == RML_CONSHDR ) {
     len += RML_HDRSTRLEN(RML_GETHDR(RML_CAR(lst)));
-	  lst = RML_CDR(lst);
+    lst = RML_CDR(lst);
   }
-  struct rml_string *str = rml_prim_mkstring(len, 1);
+  
+  /* allocate the string */
+  str = rml_prim_mkstring(len, 1);
+  if (len == 0) /* if the list is empty, return empty string! */
+  {
+    str->data[0] = '\0';     /* set the end to 0 */
+    rmlA0 = RML_TAGPTR(str); /* set the result to the tagged pointer */
+    RML_TAILCALLK(rmlSC);    /* return from the function */
+  }
+
   /* re-read the rmlA0 as it might have been moved by the GC */
   lst = rmlA0;
   while( RML_GETHDR(lst) == RML_CONSHDR ) {
@@ -78,9 +91,9 @@ RML_BEGIN_LABEL(RML__string_5fappend_5flist)
       RML_STRINGDATA(car), 
       len_car);
     len_cur += len_car;
-	  lst = RML_CDR(lst);
+	lst = RML_CDR(lst);
   }
-  str->data[len_cur+1] = '\0';
+  str->data[len_cur] = '\0';
   rmlA0 = RML_TAGPTR(str);
   RML_TAILCALLK(rmlSC);
 }
@@ -288,7 +301,7 @@ RML_END_LABEL
 RML_BEGIN_LABEL(RML__string_5fsetnth_5fstring_5fchar)
 {
   void *strold = rmlA0; /* string */
-  rml_uint_t len = RML_HDRSTRLEN(RML_GETHDR(rmlA0)); /* string lenght */
+  rml_uint_t len = RML_HDRSTRLEN(RML_GETHDR(rmlA0)); /* string length */
   rml_uint_t i = (rml_uint_t)RML_UNTAGFIXNUM(rmlA1); /* index */
   rml_uint_t ch = RML_STRINGDATA(rmlA2)[0]; /* char */
   if( i >= RML_HDRSTRLEN(RML_GETHDR(strold)) ) 
@@ -346,8 +359,25 @@ RML_END_LABEL
 
 RML_BEGIN_LABEL(RML__string_5fequal)
 {
+  /* strings pointed to by the same pointer are equal */
+  if (rmlA0 == rmlA1) 
+  { 
+    rmlA0 = RML_TRUE; 
+    RML_TAILCALLK(rmlSC); 
+  }
+  else
+  {
   char *str1 = RML_STRINGDATA(rmlA0);
   char *str2 = RML_STRINGDATA(rmlA1);
+  rml_uint_t len1 = RML_HDRSTRLEN(RML_GETHDR(rmlA0));
+  rml_uint_t len2 = RML_HDRSTRLEN(RML_GETHDR(rmlA1));
+  if ((len1 == len2) && (memcmp(str1, str2, len1+1) == 0))
+    rmlA0 = RML_TRUE;
+  else
+    rmlA0 = RML_FALSE;
+  
+  RML_TAILCALLK(rmlSC);
+  /* older implementation.
   rml_uint_t len1 = strlen(str1);
   rml_uint_t len2 = strlen(str2);
   if (len1 != len2) 
@@ -360,6 +390,8 @@ RML_BEGIN_LABEL(RML__string_5fequal)
   else
     rmlA0 = RML_FALSE;
   RML_TAILCALLK(rmlSC);
+  */
+  }
 }
 RML_END_LABEL
 
