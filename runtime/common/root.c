d7799fef5d0e4d92b99e45b8641d2f882409895e
adrpo <adrpo@324bd646-64f9-0310-949b-d3e75d9ef174>
2010-10-26 17:44:21 +0000

    - fixed the setGlobalRoot/getGlobalRoot
    git-svn-id: https://openmodelica.org/svn/MetaModelica/trunk@181 324bd646-64f9-0310-949b-d3e75d9ef174

--- runtime/common/root.c
+++ runtime/common/root.c
@@ -7,16 +7,13 @@
 /* 1024 global roots */
 void         *rml_global_roots_trail[1024] = {0};
 rml_uint_t    rml_global_roots_trail_size = 1024;
-rml_sint_t    rml_global_roots_trail_index_max = -1;
+rml_uint_t    rml_global_roots_trail_index_max = 0;
 
 /* forward my external roots */
 void rml_roots_gc(struct rml_xgcstate *state)
 {
-  if (rml_global_roots_trail_index_max >= 0) /* if not negative */
-    rml_roots_gc_callback(
-      state, 
-      rml_global_roots_trail, 
-      rml_global_roots_trail_index_max*sizeof(void*));
+  if (rml_global_roots_trail_index_max > 0) /* if not zero */
+    rml_roots_gc_callback(state, rml_global_roots_trail, rml_global_roots_trail_index_max*sizeof(void*));
 }
 
 RML_BEGIN_LABEL(RML__set_5fglobal_5froot)
@@ -49,7 +46,8 @@ RML_BEGIN_LABEL(RML__get_5fglobal_5froot)
     fprintf(stderr, "RML.getGlobalRoot\n"); fflush(stderr);
   }
   
-  if (i > rml_global_roots_trail_index_max || i >= rml_global_roots_trail_size)
+  if ((i > (rml_global_roots_trail_index_max-1)) || /* index bigger than the ones already added */
+      (i >= rml_global_roots_trail_size))           /* index bigger than our roots max */
     RML_TAILCALLK(rmlFC);
   
   rmlA0 = rml_global_roots_trail[i];
