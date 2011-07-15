b426440f2ee44b9dc5193a9df02f23e9ad32e031
sjoelund.se <sjoelund.se@324bd646-64f9-0310-949b-d3e75d9ef174>
2011-07-15 08:20:06 +0000

    - Use the same realString as omc (and same version for all platforms)
    git-svn-id: https://openmodelica.org/svn/MetaModelica/trunk@211 324bd646-64f9-0310-949b-d3e75d9ef174

--- runtime/common/real.c
+++ runtime/common/real.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <errno.h>
 #include "rml.h"
 
 /* p_mkreal.c */
@@ -339,128 +340,48 @@ RML_BEGIN_LABEL(RML__real_5fsqrt)
 RML_END_LABEL
 
 /* real_str.c */
+static const RML_DEFSTRINGLIT(_RML_LIT_NEG_INF,4,"-inf");
+static const RML_DEFSTRINGLIT(_RML_LIT_POS_INF,3,"inf");
+static const RML_DEFSTRINGLIT(_RML_LIT_NAN,3,"NaN");
+
 RML_BEGIN_LABEL(RML__real_5fstring)
 {
-#if defined(__MINGW32__) || defined(_MSC_VER)
-  int expo;
-  int count;
-  int i;
-  int frac;
-  char buf[32], *q;
-  struct rml_string *str;
-
-  /* The reason for this code is that no printf f.p. conversion
-   * modifier does exactly what we want:
-   * %f doesn't generate exponents for large-magnitude numbers
-   * %e generates exponents even for small-magnitude numbers
-   * %g does use exponents when necessary, but also omits zero fractions
-   * %#g emits zero fractions, but also emits excessive trailing zeros
-   *
-   * As a workaround, use %g but scan the output and append ".0" if no
-   * fraction or exponent was emitted.
-   */
-  sprintf(buf, "%.15g", rml_prim_get_real(rmlA0));
-  expo = 0;
-  count = 0;
-  frac = 0;
-  for(q = buf; ;) 
-  {  /* make sure it doesn't look like an int */
-    char c = *q++;
-    if( isdigit(c) ) 
-    {
-      if (expo) count++;
-        continue;
+  /* 64-bit (1+11+52) double: -d.[15 digits]E-[4 digits] = ~24 digits max.
+   * Add safety margin. */
+  static char buffer[32];
+  struct rml_string *res;
+  double r = rml_prim_get_real(rmlA0);
+  // fprintf(stderr, "\nrealString(%g)\n", r);
+  if (isinf(r) && r < 0)
+    res = RML_REFSTRINGLIT(_RML_LIT_NEG_INF);
+  else if (isinf(r))
+    res = RML_REFSTRINGLIT(_RML_LIT_POS_INF);
+  else if (isnan(r))
+    res = RML_REFSTRINGLIT(_RML_LIT_NAN);
+  else {
+    char* endptr;
+    int ix = snprintf(buffer, 32, "%.15g", r);
+    long ignore;
+    if (ix < 0)
+      RML_TAILCALLK(rmlFC);
+    errno = 0;
+    /* If it looks like an integer, we need to append .0 so it looks like real */
+    ignore = strtol(buffer,&endptr,10);
+    if (errno == 0 && *endptr == '\0') {
+      if (ix > 30)
+        RML_TAILCALLK(rmlFC);
+      buffer[ix++] = '.';
+      buffer[ix++] = '0';
+      buffer[ix] = '\0';
     }
+    res = rml_prim_mkstring(strlen(buffer), 0);
+    RML_CHECK_POINTER(res, RML__real_5fstring, "RML.realString");
 
-    if( c == '\0' && ! expo && !frac) 
-    {  /* looks like int -- append ".0" */
-      q[-1] = '.';
-      q[0] = '0';
-      q[1] = '\0';
-      break;
-    }
-    else if (c == '\0')
-    {
-      /* This makes sure that the 1.0e-/+005 is rewritten to 1.0e-/+05 like in 
-       * the cygwin version so that the testsuite works 
-       */ 
-      /* printf("buf:%s, q:%s, expo:%d, count:%d\n", buf, q, expo, count); */
-      if (expo && count >= 3 && q[-1-count] == '0') 
-      {
-        for(i=count; i>0; i--) 
-        {
-          q[-1-i] = q[-i];
-        }
-      }
-      break;
-    }
-
-    if( c == '-' || c == '+')
-      continue;
-    
-    if( c == 'e' ) 
-    {
-      expo = 1;
-      continue;
-    }
-
-    if (c == '.') frac = 1;
+    strcpy(res->data, buffer);  /* this also sets the ending '\0' */
   }
 
-  str = rml_prim_mkstring(strlen(buf), 0);
-  RML_CHECK_POINTER(str, RML__real_5fstring, "RML.realString");
-
-  strcpy(str->data, buf);  /* this also sets the ending '\0' */
-  rmlA0 = RML_TAGPTR(str);
+  rmlA0 = RML_TAGPTR(res);
   RML_TAILCALLK(rmlSC);
-
-#else /* Linux or other stuff */
-
-  char buf[32], *q;
-  struct rml_string *str;
-
-  /* The reason for this code is that no printf f.p. conversion
-   * modifier does exactly what we want:
-   * %f doesn't generate exponents for large-magnitude numbers
-   * %e generates exponents even for small-magnitude numbers
-   * %g does use exponents when necessary, but also omits zero fractions
-   * %#g emits zero fractions, but also emits excessive trailing zeros
-   *
-   * As a workaround, use %g but scan the output and append ".0" if no
-   * fraction or exponent was emitted.
-   */
-  sprintf(buf, "%.15g", rml_prim_get_real(rmlA0));
-  for(q = buf; ;) 
-  {  /* make sure it doesn't look like an int */
-    char c = *q++;
-    if( isdigit(c) )
-      continue;
-    if( c == '\0' ) 
-    {  /* looks like int -- append ".0" */
-      q[-1] = '.';
-      q[0] = '0';
-      q[1] = '\0';
-      break;
-    }
-    
-    if( c == '-' )
-      continue;
-    /* If we get here we found
-     * '.', indicating a fraction (ok),
-     * 'e', indicating an exponent (ok),
-     * or something else, probably indicating nan or inf (bad).
-     * In either case, leave the string as-is.
-     */
-    break;
-  }
-  
-  str = rml_prim_mkstring(strlen(buf), 0);
-  RML_CHECK_POINTER(str, RML__real_5fstring, "RML.realString");
-
-  strcpy(str->data, buf);  /* this also sets the ending '\0' */
-  rmlA0 = RML_TAGPTR(str);
-  RML_TAILCALLK(rmlSC);
-#endif
 }
 RML_END_LABEL
 
