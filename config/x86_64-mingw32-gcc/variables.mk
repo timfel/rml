f34d7525103e767321d3249f6d7b78a6f02885ff
adrpo <adrpo@324bd646-64f9-0310-949b-d3e75d9ef174>
2013-11-22 04:15:54 +0000

    - better support x86_64-mingw32-gcc (mingw64) - configure using: CC=x86_64-w64-mingw32-gcc ./configure
    git-svn-id: https://openmodelica.org/svn/MetaModelica/trunk@261 324bd646-64f9-0310-949b-d3e75d9ef174

--- config/x86_64-mingw32-gcc/variables.mk
+++ config/x86_64-mingw32-gcc/variables.mk
@@ -1,11 +1,11 @@
-# variables for Intel x86 / Mingw32 / gcc
-CC=gcc
+# variables for Intel x86_64 / Mingw32 / gcc
+CC=x86_64-w64-mingw32-gcc
 CFLAGS=
 COFLAGS=-O3 -fomit-frame-pointer
 CPFLAGS=-fno-omit-frame-pointer -pg
 CGFLAGS=-g -DRML_DEBUG 
 CTFLAGS=-DRML_TRACE 
 RANLIB=ranlib
-CPP=gcc -x c -E -ansi
-AS=as
-LD=ld
+CPP=x86_64-w64-mingw32-gcc -x c -E -ansi
+AS=x86_64-w64-mingw32-as
+LD=x86_64-w64-mingw32-ld
